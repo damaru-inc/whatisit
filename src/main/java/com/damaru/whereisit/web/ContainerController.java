a54c41d34519943342fff9d6e7b906d22dd9442a
Michael Davis <michael@damaru.com>
2023-12-30 18:04:28 -0500

    Changed the scope of newContainer.

--- src/main/java/com/damaru/whereisit/web/ContainerController.java
+++ src/main/java/com/damaru/whereisit/web/ContainerController.java
@@ -3,11 +3,9 @@ package com.damaru.whereisit.web;
 import java.util.logging.Logger;
 
 import javax.enterprise.inject.Model;
-import javax.enterprise.inject.Produces;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-import javax.inject.Named;
 
 import com.damaru.whereisit.model.Container;
 import com.damaru.whereisit.model.Room;
@@ -24,13 +22,10 @@ public class ContainerController {
     @Inject
     private FacesContext facesContext;
 
-    @Named
-    @Produces
     private Container newContainer = new Container();
     
     private Long selectedRoomId = 0L;
-    
-    
+        
     public String create() {
         try {
             log.info("Saving " + newContainer + " with selectedRoomId " + selectedRoomId);
@@ -47,6 +42,13 @@ public class ContainerController {
         return "container";
     }
 
+    public Container getNewContainer() {
+        return newContainer;
+    }
+
+    public void setNewContainer(Container newContainer) {
+        this.newContainer = newContainer;
+    }
 
     public Long getSelectedRoomId() {
         return selectedRoomId;
@@ -56,7 +58,4 @@ public class ContainerController {
     public void setSelectedRoomId(Long selectedRoomId) {
         this.selectedRoomId = selectedRoomId;
     }
-
-    
-
 }
